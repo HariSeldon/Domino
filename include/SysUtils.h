b776db787fde3c10dd9c2f67ccfb5193d4356935
Hari Seldon <alberto.magni86@gmail.com>
2015-10-18 17:18:00 +0200

    Remove timing function.

--- include/SysUtils.h
+++ include/SysUtils.h
@@ -56,16 +56,3 @@ inline void checkOpenGLError(
 }
 
 void dumpTextureToFile(GLuint inputTexture);
-
-auto timeFunction = [](auto &&function, auto&&... params) {
-  // Start timer.
-  auto begin = std::chrono::system_clock::now();
-  std::forward<decltype(function)>(function)(
-      std::forward<decltype(params)>(params)...
-      );
-  // Stop timer.
-  auto end = std::chrono::system_clock::now();
-  // Print execution time.
-  auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count();
-  return duration;
-};
